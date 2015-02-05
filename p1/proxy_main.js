51b2a47df3c4d5951b9a304825f8575023861f65
Brandon Dalesandro <brandonadalesandro@gmail.com>
2015-02-05 13:58:39 -0800

    extended functionality, and added comments for clarity

--- p1/proxy_main.js
+++ p1/proxy_main.js
@@ -7,79 +7,117 @@ var PORT = '1337';
 
 net.createServer({allowHalfOpen: true}, function(sock) {
 	sock.on('data', function(data) {
+
+        // parse the request
 		var req_array = data.toString().split('\r\n');
 		var req_line = req_array[0].split(' ');
 		var req_args = parseArgs(req_array.slice(1, req_array.length));
         var req_url = url.parse(req_line[1]);
 
-        console.log(req_array[0] + "\nsock bytes read: " + sock.bytesRead);
+        // if we have no port, it's probably http
         if (req_url.port === null) {
             req_url.port = 80;
         }
 
-        var client = null;
-        var sendStuff = null;
+        // set up vars for scope
+        var sendStuff = getRequestString(req_line, req_args);
         var host = null;
         var port = null;
+
 		// open a TCP socket to them
 		if (req_line[0] === "CONNECT") {
-			sendStuff = getRequestString(req_line, req_args);
+            // set the connection/proxyconnection to kee-alive
 			sendStuff.Connection = "keep-alive";
-			client = new net.Socket({allowHalfOpen: true});
+            sendStuff["Proxy-connection"] = "keep-alive";          
+            console.log("CONNECT: sending\n" + sendStuff);
+            
+            // determine the host & port, then create a connection
             host = req_line[1].split(':')[0];
             port = req_line[1].split(':')[1];
+            var con_client = net.createConnection(port, host);
             console.log("CONNECT: host=" + host + ":" + port);
-			client.connect(port, host, function() {
+			con_client.on('connect', function() {
+                console.log("CONNECT: success when connecting to host=" + host + ":" + port);
 				sock.write("HTTP/1.1 200 OK");
+                con_client.write(sendStuff);
 
                 // browser => server
                 sock.on('data', function(data) {
-                    client.write(data);
+                    console.log("CONNECT: recieving data from browser");
+                    con_client.write(data);
                 });
 
                 // server => browser
-			    client.on('data', function(data) {
+			    con_client.on('data', function(data) {
+                    console.log("CONNECT: recieving data from host=" + host + ":" + port);
 			    	sock.write(data);
 			    });
 
-			    client.on('end', function() {
+			    con_client.on('end', function() {
 			    	sock.end();
 			    });
 
-			    client.on('close', function() {
+			    con_client.on('close', function() {
 			    	sock.end();
 			    });
 			});
 
+            con_client.on('error', function(err) {
+                console.log("CONNECT: error when connecting to host=" + host + ":" + port);
+                console.log(err);
+                sock.end("HTTP/1.1 502 Bad Gateway");
+            });
+
+            con_client.setKeepAlive(enabled=true, 1000);
+
 		// just relay the request
 		} else {
-            sendStuff = getRequestString(req_line, req_args);
-			client = new net.Socket({allowHalfOpen: true});
+            req_args.Connection = "close";
+            req_args['Proxy-connection'] = "close";
+			var client = new net.Socket({allowHalfOpen: true});
             port = req_url.port;
             host = req_url.hostname;
             console.log("PACKET: host=" + host + ":" + port);
+
 			client.connect(port, host, function() {
-				client.write(sendStuff + '\r\n');
-			    client.on('data', function(data) {
-			    	sock.write(data);
-			    });
 
-			    client.on('end', function() {
-			    	sock.end();
-			    });
+                client.on('data', function(data) {
+                    sock.write(data);
+                });
 
-			    client.on('close', function() {
-			    	sock.end();
-			    });
+                client.on('end', function() {
+                    sock.end();
+                });
+
+                client.on('close', function() {
+                    sock.end();
+                });
                 
                 // Browser has signalled end, relay this to the server
                 sock.on('end', function() {
                     console.log("browser ended, bytes read: " + sock.bytesRead);
                     client.end();
                 });
+
+				client.write(sendStuff + '\r\n');
+
 			});
+
+            client.on('error', function(err) {
+                console.log("PACKET: error");
+                console.log(err);
+            });
 		}
-	});
+	
+        sock.on('error', function(err) {
+            console.log("SOCK: error");
+            console.log(err);
+            if(client !== null) 
+                client.end();
+            sock.end();
+        });
+    });
+
 }).listen(PORT, HOST);
 
 console.log("TCP server listening on port " + PORT);
