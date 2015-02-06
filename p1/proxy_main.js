d58d3582850fe6bcc9381c967f4a71025b869570
shampson <shampson@uw.edu>
2015-02-05 23:55:00 -0800

    Connection works, regular stuff seems to work (but still writing to socket after close errors)

--- p1/proxy_main.js
+++ p1/proxy_main.js
@@ -9,39 +9,45 @@ net.createServer({allowHalfOpen: true}, function(sock) {
     var isTunnel = false;
     var tunnel  = null;
     var ref = 0;
+    var browserDone = false;
 
 	sock.on('data', function(data) {
         //console.log("sockets: " + ref);
+        // Relay data to server if tunneling 
+        // assumes if a socket is opened for a connection it is only used for tunneling
         if (isTunnel) {
-            console.log();
-            console.log("------------------------SENDING THRU TUNNEL ----------------");
-            console.log();
+            //console.log("Tunnel data to server");
             tunnel.write(data);
             return;
         }
 
-        // parse the request 
-        var everything = data.toString().split('\r\n\r\n');
+        // parse the request (header ends on empty line)
+        var header_and_data = data.toString().split('\r\n\r\n');
 		var req_array = null;
         var req_data = null;
-        if (everything.length == 2) {
-            req_array = everything[0].replace(/\r/gm, '');
-            req_data = everything[1];
+        if (header_and_data.length == 2) {
+            req_array = header_and_data[0].replace(/\r/gm, '');
+            req_data = header_and_data[1];
         } else {
+            // Data is just header
             req_array = data.toString();
         }
-        //console.log(everything);
-        //console.log("is tunnel? " + isTunnel);
         req_array = req_array.split('\n');
 		var req_line = req_array[0].split(' ');
 		var req_args = parseArgs(req_array.slice(1, req_array.length));
+        console.log(req_line[1]);
         var req_url = url.parse(req_line[1]);
+        
         // if we have no port, it's probably http
+        var is_https = (req_array.indexOf("https") > -1);
         if (req_url.port === null) {
-            req_url.port = 80;
+            if (is_https) {
+                req_url.port = 443;
+            } else {
+                req_url.port = 80;
+            }
         }
-        //console.log(req_line);
-        console.log(req_array[0]);
+        
         // set up vars for scope
         var sendStuff = getRequestString(req_line, req_args);
         var host = null;
@@ -49,86 +55,96 @@ net.createServer({allowHalfOpen: true}, function(sock) {
 
 		// open a TCP socket to them
 		if (req_line[0] === "CONNECT") {
-
+            console.log();
+            console.log("Establishing CONNECT");
+            console.log();
             // set the connection/proxyconnection to kee-alive        
-            //console.log("CONNECT: sending\n" + data);
             isTunnel = true;
             // determine the host & port, then create a connection
             host = req_line[1].split(':')[0];
             port = req_line[1].split(':')[1];
-            tunnel = net.createConnection(port, host);
+            tunnel = new net.Socket({allowHalfOpen: true});
             //console.log("CONNECT: host=" + host + ":" + port);
-			tunnel.on('connect', function() {
+			
+            // Attempting to connect the server
+            tunnel.connect(port, host, function() {
                 //console.log("CONNECT: success when connecting to host=" + host + ":" + port);
 				sock.write("HTTP/1.1 200 OK\r\n\r\n");
-                tunnel.write(req_data);
 
                 // server => browser
 			    tunnel.on('data', function(data) {
                     //console.log("CONNECT: recieving data from host=" + host + ":" + port);
-			    	sock.write(data);
+			    	//console.log("Tunnel data to browser");
+                    sock.write(data);
 			    });
 
 			    tunnel.on('end', function() {
+                    console.log("(end)Server closed its tunnel");
 			    	sock.end();
-                    isTunnel = false;
-			    });
+                });
+            });
 
-			    tunnel.on('close', function() {
-			    	sock.end();
-                    isTunnel = false;
-			    });
-			});
+            sock.on('end', function() {
+                //console.log("browser ended, bytes read: " + sock.bytesRead);
+                console.log("browser closed connection");
+                tunnel.end();
+            });
 
             tunnel.on('error', function(err) {
                 //console.log("CONNECT: error when connecting to host=" + host + ":" + port);
                 //console.log(err);
+                console.log("Error Establishing tunnel");
                 sock.end("HTTP/1.1 502 Bad Gateway\r\n\r\n");
             });
 
             tunnel.setKeepAlive(enabled=true, 1000);
 
-		// just relay the request
-		} else {
+        // just relay the request
+        } else {
+            ref++;
+            if (ref > 1) {
+                console.log("------------sock has " + ref + " requests-----------");
+            }
+            console.log(req_array[0]);
             req_args.Connection = "close";
             req_args['Proxy-connection'] = "close";
 			var client = new net.Socket({allowHalfOpen: true});
             port = req_url.port;
             host = req_url.hostname;
-            //console.log("PACKET: host=" + host + ":" + port);
+            console.log("Client: host=" + host + ":" + port);
 
-			client.connect(port, host, function() {
-
+            client.connect(port, host, function() {
+                
+                client.end(sendStuff + '\r\n');
+                
                 client.on('data', function(data) {
                     sock.write(data);
                 });
 
                 client.on('end', function() {
-                    //sock.end();
+                    ref--;
+                    if (ref == 0) {
+                        console.log("server closed its connection(end)");
+                        sock.end();
+                    }
                 });
 
-                client.on('close', function() {
-                    sock.end();
-                });
-                
                 // Browser has signalled end, relay this to the server
                 sock.on('end', function() {
                     //console.log("browser ended, bytes read: " + sock.bytesRead);
+                    browserDone = true;
                     client.end();
                 });
-
-				client.write(sendStuff + '\r\n');
-
-			});
+            });
 
             client.on('error', function(err) {
-                //console.log("PACKET: error");
-                //console.log(err);
+                console.log("Server socket: error");
+                console.log(err);
             });
-		}
-	
+        }
+
         sock.on('error', function(err) {
-            //console.log("SOCK: error");
+            console.log("Browser socket: error");
             console.log(err);
         });
     });
