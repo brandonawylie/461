8007f6b89a18ff2575baa5493584a87128e7a546
shampson <shampson@uw.edu>
2015-03-02 18:06:18 -0800

    registration working

--- p2/main.js
+++ p2/main.js
@@ -7,9 +7,11 @@ var url  = require('url');
 var util = require('util');
 var spawn = require('child_process').spawn;
 
-var TAG = "main.js: ";
-var PORT = 1337
-
+var routerNumber = Math.floor((Math.random() * 9999) + 1);
+var TAG = routerNumber + ": main.js: ";
+var PORT = 1337;
+var torName = "Tor61Router";
+var groupNum = 5316;
 var torRegistrations = '';
 
 var server = net.createServer({allowHalfOpen: true}, function(incomingSocket) {
@@ -21,6 +23,7 @@ var server = net.createServer({allowHalfOpen: true}, function(incomingSocket) {
 getTorRegistration(function(data) {
     if (data === null) {
         // error
+        util.log(TAG + "errored getting registrations")
     }
     torRegistrations = data;
     //make circuit
@@ -28,22 +31,20 @@ getTorRegistration(function(data) {
 }).then(function() {
     server.listen(PORT, function() {
         util.log(TAG + "TCP Server Bound to port " + PORT);
-        // Register router
-        // TODO: get name & int data
-        registerRouter(PORT, router_name, int_data);
+        registerRouter(PORT);
     });
 });
 
 function createCircuit(data) {
     util.log(TAG + "Creating circuit...");
-    
-
 }
 
-function registerRouter(port, router_name, int_data) {
+function registerRouter(port) {
     var spawn = require('child_process').spawn;
+    var instanceNum = Math.floor((Math.random() * 9999) + 1);
+    router_name = torName + "-" + groupNum + instanceNum;
     // Currently has dummy registration info
-    var regClient = spawn('python', ['./registration_client.py', port, router_name, int_data]);
+    var regClient = spawn('python', ['./registration_client.py', port, router_name, routerNumber]);
     
     function endChild() {
         regClient.kill('SIGINT');
