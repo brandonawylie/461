b84a60a81cf6970f4b76bc8c2dd8d209c1348df4
Brandon Dalesandro <brandonadalesandro@gmail.com>
2015-03-04 13:49:11 -0800

    added some logging statements, got it to run

--- p2/main.js
+++ p2/main.js
@@ -7,12 +7,20 @@ var url  = require('url');
 var util = require('util');
 var spawn = require('child_process').spawn;
 
-var routerNumber = Math.floor((Math.random() * 9999) + 1);
-var TAG = routerNumber + ": main.js: ";
 var PORT = 1337;
+
+// registations
+var torRegistrations = '';
+
+// register ourself
 var torName = "Tor61Router";
 var groupNum = 5316;
-var torRegistrations = '';
+var instanceNum = Math.floor((Math.random() * 9999) + 1);
+var router_name = torName + "-" + groupNum + "-" + instanceNum;
+var routerNumber = Math.floor((Math.random() * 9999) + 1);
+
+// TAG ourselves to log
+var TAG = router_name + ": main.js: ";
 
 var server = net.createServer({allowHalfOpen: true}, function(incomingSocket) {
     util.log(TAG + "Received Incoming Socket from host " + incomingSocket.remoteAddress + ":" + incomingSocket.remotePort);
@@ -20,42 +28,44 @@ var server = net.createServer({allowHalfOpen: true}, function(incomingSocket) {
     
 });
 
-getTorRegistration(function(data) {
+getTorRegistrations(function(data) {
     if (data === null) {
         // error
-        util.log(TAG + "errored getting registrations")
+        util.log(TAG + "errored getting registrations");
     }
     torRegistrations = data;
-    //make circuit
-    createCircuit(data);
-}).then(function() {
+    util.log(TAG + "Registrations recieved: \n" + data);
     server.listen(PORT, function() {
         util.log(TAG + "TCP Server Bound to port " + PORT);
         registerRouter(PORT);
+        createCircuit(data);
     });
 });
+
 
 function createCircuit(data) {
     util.log(TAG + "Creating circuit...");
 }
 
 function registerRouter(port) {
-    var spawn = require('child_process').spawn;
-    var instanceNum = Math.floor((Math.random() * 9999) + 1);
-    router_name = torName + "-" + groupNum + instanceNum;
+
     // Currently has dummy registration info
     var regClient = spawn('python', ['./registration_client.py', port, router_name, routerNumber]);
-    
+
+    util.log(TAG + "registering: in progress");
     function endChild() {
         regClient.kill('SIGINT');
         process.exit(0);
     }
 
     regClient.stdout.on('data', function(data) {
+        util.log(TAG + "registering got stdin with data: " + data);
+        util.log(TAG + "got stdin with data: " + data);
         console.log(data.toString());
     });
     
     regClient.stderr.on('data', function(data) {
+        util.log(TAG + "got stderr with data: " + data);
         console.log(data.toString());
     });
 
